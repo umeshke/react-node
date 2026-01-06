9f7e193436b859720ae665e46e5a804be3d89e76
Umesh Kumar Keshri <umeshke@cybage.com>
2026-01-06 18:44:46 +0530

    Minor changes

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -8,7 +8,7 @@ const Signup = () => {
 
   return (
     <form onSubmit={handleSubmit(signup)} className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
-      <input {...register('name')} placeholder="Name" className="w-full p-3 border rounded mb-4" />
+      <input {...register('name')} type="text" placeholder="Name" className="w-full p-3 border rounded mb-4" />
       <input {...register('email')} type="email" placeholder="Email" className="w-full p-3 border rounded mb-4" />
       <input {...register('password')} type="password" placeholder="Password" className="w-full p-3 border rounded mb-4" />
       <button type="submit" disabled={loading} className="w-full bg-blue-500 text-white py-3 rounded hover:bg-blue-600">
